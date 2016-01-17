2a1fed16153e39b3dbc5a5985f1b3f3e01952ab2
trebloc <ctse77@alum.berkeley.edu>
2016-01-16 21:59:34 -0800

    addding route for show

--- trial2/config/routes.rb
+++ trial2/config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
   get "/articles/new", to: "articles#new", as: "new_article"  
   get "/users/:user_id/articles", to: "articles#index", as: "user_articles"
   get "/users/:user_id/articles/new", to: "articles#new", as: "new_article" 
+  post "/users/:user_id/articles/:id", to: "articles#create"
+  get  "/users/:user_id/articles/:id", to: "articles#show" 
 end
