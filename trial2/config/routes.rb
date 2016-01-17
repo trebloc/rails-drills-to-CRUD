7793b015438d1c6032e2767da41999ea4791470f
trebloc <ctse77@alum.berkeley.edu>
2016-01-16 20:24:13 -0800

    added new users sign up

--- trial2/config/routes.rb
+++ trial2/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "users#index"
-  
+
   get "/users", to: "users#index", as: "users"
   get "/users/new", to: "users#new", as: "new_user"
+  post "/users", to: "users#create"  
 end
