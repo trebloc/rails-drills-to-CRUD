700c6f991d3314b57160955fcfa6e4f4edc45dc1
trebloc <ctse77@alum.berkeley.edu>
2016-01-16 21:18:07 -0800

    finished users

--- trial2/app/controllers/users_controller.rb
+++ trial2/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   # grab the users
-	def index
+  def index
     @users = User.all
     render :index
   end
@@ -11,13 +11,21 @@ class UsersController < ApplicationController
   end
 
   def create
-  	user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
-  	@user = User.create(user_params)
- 		redirect_to root_path
- 		end 	
+    @user = User.create(user_params) # calls user_params method
+    login(@user)
+    redirect_to @user
+  end
 
- 	def method_name
- 		@user = User.find_by_id(params[:id])
- 		render :show		
- 	end		
+  
+  def show
+    @user = User.find_by_id(params[:id])
+    render :show
+  end  
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end     
 end
--- trial2/config/routes.rb
+++ trial2/config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
 
   get "/users", to: "users#index", as: "users"
   get "/users/new", to: "users#new", as: "new_user"
-  post "/users", to: "users#create"  
-  get "/users/:id", to: "users#show", as: "user" 
+  post "/users", to: "users#create"
+  get "/users/:id", to: "users#show", as: "user"
+  get "/login", to: "sessions#new"
+  get "/logout", to: "sessions#destroy" # <-- strictly speaking this isn't RESTful (it should be a DELETE not GET), but it's super conveient to do it this way
+  post "/sessions", to: "sessions#create"
   
-  get "/login", to: "sessions#new"  
-  post "/sessions", to: "sessions#create"   
+  get "/login", to: "sessions#new"
+  get "/logout", to: "sessions#destroy" # <-- strictly speaking this isn't RESTful (it should be a DELETE not GET), but it's super conveient to do it this way
+  post "/sessions", to: "sessions#create" 
 end
