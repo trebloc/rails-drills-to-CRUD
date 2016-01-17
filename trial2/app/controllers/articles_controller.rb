ec2448d14e70896758f64595b9b4b9679b7c9e85
trebloc <ctse77@alum.berkeley.edu>
2016-01-16 22:11:56 -0800

    added destroy

--- trial2/app/controllers/articles_controller.rb
+++ trial2/app/controllers/articles_controller.rb
@@ -1,25 +1,24 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
-
     render :index
   end
 
   def new
-    @article = Article.new
-    render :new
-  end 
+	@article = Article.new
+    render :new  	
+  end
 
- def create
+  def create
     article_params = params.require(:article).permit(:title, :content)
     @article = Article.create(article_params)
-    redirect_to "/users/#{params[:user_id]}/articles/#{params[:article_id]}"
-  end 
 
- def show
+    redirect_to "/users/#{params[:user_id]}/articles/#{params[:article_id]}"
+  end  
+  def show
     @article = Article.find(params[:id])
     render :show
-  end
+  end  
 
   def edit
     @article = Article.find_by_id(params[:id])
@@ -30,5 +29,11 @@ class ArticlesController < ApplicationController
     @article = Article.find_by_id(params[:id])
     @article.update_attributes(article_params)
     redirect_to 
-   end      	
+   end
+
+def destroy
+    @article = Article.find_by_id(params[:id])
+    @article.destroy
+    redirect_to "/users/#{params[:user_id]}/articles"
+  end     
 end
--- trial2/config/routes.rb
+++ trial2/config/routes.rb
@@ -9,15 +9,12 @@ Rails.application.routes.draw do
   get "/logout", to: "sessions#destroy" # <-- strictly speaking this isn't RESTful (it should be a DELETE not GET), but it's super conveient to do it this way
   post "/sessions", to: "sessions#create"
   
-  get "/login", to: "sessions#new"
-  get "/logout", to: "sessions#destroy" # <-- strictly speaking this isn't RESTful (it should be a DELETE not GET), but it's super conveient to do it this way
-  post "/sessions", to: "sessions#create" 
-
-  get "/articles/new", to: "articles#new", as: "new_article"  
   get "/users/:user_id/articles", to: "articles#index", as: "user_articles"
   get "/users/:user_id/articles/new", to: "articles#new", as: "new_article" 
   post "/users/:user_id/articles/:id", to: "articles#create"
-  get  "/users/:user_id/articles/:id", to: "articles#show" 
+  get  "/users/:user_id/articles/:id", to: "articles#show"  
   get "/users/:user_id/articles/:id/edit", to: "articles#edit", as: "edit_article"
   patch "/users/:user_id/articles/:id", to: "articles#update"  
+  delete "/users/:user_id/articles/:id", to: "articles#destroy" 
 end
+
