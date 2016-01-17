d6b2c1a7a20b7a7fdff1fde42a89444e96bc59ae
trebloc <ctse77@alum.berkeley.edu>
2016-01-16 16:10:20 -0800

    adding new article route

--- trial1/config/routes.rb
+++ trial1/config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   get "/logout", to: "sessions#destroy" # <-- strictly speaking this isn't RESTful (it should be a DELETE not GET), but it's super conveient to do it this way
   post "/sessions", to: "sessions#create"
 
-
-
+  get "/articles", to: "articles#index"
+  get "/articles/new", to: "articles#new", as: "new_article"  
 end
