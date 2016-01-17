3d16d0dc1ee59916b16b0e5bf6c8d2432ef41c7b
trebloc <ctse77@alum.berkeley.edu>
2016-01-16 20:21:21 -0800

    added new users to the user controller

--- trial2/app/controllers/users_controller.rb
+++ trial2/app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   # grab the users
-  def index
+	def index
     @users = User.all
     render :index
+  end
+
+  def new
+    @user = User.new
+    render :new
   end
 end
