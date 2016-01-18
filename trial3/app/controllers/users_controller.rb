4e7d72df16c462c716a1de212085d970068e49bf
trebloc <ctse77@alum.berkeley.edu>
2016-01-17 22:24:12 -0800

    added create method

--- trial3/app/controllers/users_controller.rb
+++ trial3/app/controllers/users_controller.rb
@@ -8,4 +8,11 @@ class UsersController < ApplicationController
   	@user = User.new
   	render :new
   end
+
+  def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    @user = User.create(user_params)
+
+    redirect_to root_path
+  end  
 end
