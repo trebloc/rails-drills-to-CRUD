9317ddb6397dffff7a3d20d907f972be5ba81584
trebloc <ctse77@alum.berkeley.edu>
2016-01-17 23:11:40 -0800

    added a private method for the users controller

--- trial3/app/controllers/users_controller.rb
+++ trial3/app/controllers/users_controller.rb
@@ -1,23 +1,30 @@
 class UsersController < ApplicationController
   def index
-  	@users = User.all
-  	render :index
+    @users = User.all
+    render :index
   end
 
   def new
-  	@user = User.new
-  	render :new
+    @user = User.new
+    render :new
   end
 
   def create
-    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
-    @user = User.create(user_params)
-    login(@user) # <-- login the user
-    redirect_to @user # <-- go to show
+    @user = User.create(user_params) # calls user_params method
+    login(@user)
+    redirect_to @user
   end
 
+  
   def show
     @user = User.find_by_id(params[:id])
-    render :show 
-  end
+    render :show
+  end  
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end     
 end
