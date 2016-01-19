22d39d594fcdfdf684b6eb6ccd74ad6140cb74c1
trebloc <ctse77@alum.berkeley.edu>
2016-01-19 00:18:32 -0800

    adding some styling with bootstrap

--- crud_app3/app/controllers/comments_controller.rb
+++ crud_app3/app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @album = Album.find(params[:album_id])
+    @comment = @album.comments.create(comment_params)
+    redirect_to album_path(@album)
+  end
+ 
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
