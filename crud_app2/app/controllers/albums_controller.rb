bc6e84c86a85c93ec8dca1f997d5c8fe28403a44
trebloc <ctse77@alum.berkeley.edu>
2016-01-18 20:31:59 -0800

    finished crud for albums

--- crud_app2/app/controllers/albums_controller.rb
+++ crud_app2/app/controllers/albums_controller.rb
@@ -1,4 +1,44 @@
 class AlbumsController < ApplicationController
-	def new
+  def index
+    @albums = Album.all
+    render :index
+  end	
+
+  def new
+		@album = Album.new
+		render :new  	
+  end
+  
+	def create
+	  @album = Album.new(album_params)
+	  @album.save
+    redirect_to albums_path
 	end
-end
+
+	def show
+		@album = Album.find(params[:id])
+		render :show
+	end
+
+  def edit
+    @album = Album.find_by_id(params[:id])
+  end	
+
+	def update
+    @album = Album.find_by_id(params[:id])
+    @album.update_attributes(album_params)
+    render :show
+  end  
+
+  def destroy
+    @album = Album.find_by_id(params[:id])
+    @album.destroy
+    redirect_to albums_path
+  end 
+
+ 
+	private
+	  def album_params
+	    params.require(:album).permit(:artist, :album)
+	  end
+	end  
--- crud_app2/config/routes.rb
+++ crud_app2/config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   get "/albums", to: "albums#index", as: "albums"
   get "/albums/new", to: "albums#new", as: "new_album"
   post "/albums", to: "albums#create"
-  get "/albums/:id", to: "albums#show", as: "album"
+  get "/albums/:id", to: "albums#show", as: "show_album"
   get "/albums/:id/edit", to: "albums#edit", as: "edit_album"
   patch "/albums/:id", to: "albums#update"
-  delete "/albums/:id", to: "albums#destroy"
+  delete "/albums/:id", to: "albums#destroy", as: "delete_album"
+
 end
