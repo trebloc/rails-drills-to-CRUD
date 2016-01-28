08fee098e5185e74d556fbdc41cec62340ebef40
trebloc <ctse77@alum.berkeley.edu>
2016-01-27 19:29:59 -0800

    deleted extra crud_app3 folder

--- crud_app/crud_app3/app/assets/javascripts/application.js
+++
@@ -1,5 +0,0 @@
-//= require_self
-//= require jquery
-//= require jquery_ujs
-//= require twitter/bootstrap/alert
-//= require twitter/bootstrap/dropdown
--- crud_app/crud_app3/app/models/setting.rb
+++
@@ -1,5 +0,0 @@
-# -*- encoding : utf-8 -*-
-class Setting < Settingslogic
-  source "#{Rails.root}/config/config.yml"
-  namespace Rails.env
-end
